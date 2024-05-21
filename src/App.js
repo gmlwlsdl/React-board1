6da1794f5b1b4236181b9be4436a3ad2151dd888
gmlwlsdl <heejin094@naver.com>
2024-05-21 22:05:10 +0900

    netlify 흰 화면 err

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import SigninDone from './pages/sign/signIn/done';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="{process.env.PUBLIC_URL">
         <div id="wrap">
           <Nav />
           <Routes>
